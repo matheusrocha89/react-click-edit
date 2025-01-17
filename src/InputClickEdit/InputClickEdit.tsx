7a182a3cc246dd6d89351e8f8060b9554d710de5
Matheus Rocha <matheus.rocha89@gmail.com>
2025-01-17 10:43:55 +0100

    feat: create input click edit component
    the first code of the component logic

--- src/InputClickEdit/InputClickEdit.tsx
+++ src/InputClickEdit/InputClickEdit.tsx
@@ -1,9 +1,80 @@
+import { useState } from "react";
+import cn from "classnames";
+
+import styles from "./InputClickEdit.module.css";
+
 type InputClickEditProps = {
   className?: string;
+  isEditing: boolean;
+  inputClassName?: string;
+  editButtonClassName?: string;
+  editWrapperClassName?: string;
+  displayText?: string;
+  saveButtonLabel?: React.ReactNode;
+  editButtonLabel?: React.ReactNode;
+  label?: string;
+  onEditButtonClick?: () => void;
+  onInputChange?: (value: string) => void;
+  onSaveButtonClick?: () => void;
 };
 
-const InputClickEdit = () => {
-  return null;
+const InputClickEdit = ({
+  className,
+  inputClassName,
+  editButtonClassName,
+  editWrapperClassName,
+  displayText,
+  isEditing = false,
+  saveButtonLabel = "Save",
+  editButtonLabel = "Edit",
+  label,
+  onEditButtonClick,
+  onInputChange,
+  onSaveButtonClick,
+}: InputClickEditProps) => {
+  const [editing, setEditing] = useState<boolean>(isEditing);
+  const onEditClick = () => {
+    setEditing(true);
+    onEditButtonClick?.();
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onInputChange?.(e.target.value);
+  };
+
+  const handleSave = () => {
+    setEditing(false);
+    onSaveButtonClick?.();
+  };
+
+  return (
+    <div className={className}>
+      {editing ? (
+        <div className={cn(styles.editWrapper, editWrapperClassName)}>
+          {label ? (
+            <label>
+              {label}
+              <input
+                className={inputClassName}
+                onChange={onChange}
+                type="text"
+              />
+            </label>
+          ) : (
+            <input className={inputClassName} onChange={onChange} type="text" />
+          )}
+          <button onClick={handleSave}>{saveButtonLabel}</button>
+        </div>
+      ) : (
+        <div>
+          <span>{displayText}</span>
+          <button className={editButtonClassName} onClick={onEditClick}>
+            {editButtonLabel}
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export { InputClickEdit };
