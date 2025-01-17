b70455f0499c14be343aef2f4d8e77f556994253
Matheus Rocha <matheus.rocha89@gmail.com>
2025-01-17 10:44:45 +0100

    chore: create playground folder
    This folder is a vite project so we can test the component while we are developing

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@nobrainers/react-click-edit": path.resolve(
+        __dirname,
+        "../src/index.ts"
+      ),
+    },
+  },
+  server: {
+    watch: {
+      ignored: ["!**/src/**"],
+    },
+  },
 });
