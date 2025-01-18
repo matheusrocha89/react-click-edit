a8cba69413cd302579d415f15a89857740608365
Matheus Rocha <matheus.rocha89@gmail.com>
2025-01-18 11:01:47 +0100

    chore: testing new properties

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <InputClickEdit onInputChange={handleChange} value={value} />
+        <InputClickEdit onInputChange={handleChange} value={value} showIcons />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
